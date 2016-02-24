a743a32ec6ab293e074d91bd3f155f9d5dbddc5e
Christopher Wood <christopherwood07@gmail.com>
2016-02-24 09:50:29 -0800

    Setup script

--- setup.sh
+++ setup.sh
@@ -1,14 +1,11 @@
 #!/bin/zsh
 
-alias check="ruby `pwd`/src/check.rb"
-
-#echo >&2 "Calling function '$1' with $# arguments"
-#echo >&2 "function '$1' returned with status $ret"
-
+# Decoration (for zsh)
 decorate() 
   functions[$1]='
     () { check -l '$@' && '$functions[$1]'; } "$@"
     local ret=$?
     return $ret'
 
+alias check="ruby `pwd`/src/check.rb"
 gc() { args="'$*'"; check -l $args; git "$@"; }
