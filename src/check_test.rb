267175be1237f31c542cf8636e9eea08cbb2d6b0
Christopher Wood <christopherwood07@gmail.com>
2016-03-01 08:25:31 -0800

    unit test stub

---
+++ src/check_test.rb
@@ -0,0 +1,18 @@
+require 'test/unit'
+
+class RepositoryTest < Test::Unit::TestCase
+    # TODO
+end
+
+class CheckpointBoundaryTest < Test::Unit::TestCase
+    # TODO
+end
+
+class CheckpointEntryTest < Test::Unit::TestCase
+    # TODO
+end
+
+class CheckpointerTest < Test::Unit::TestCase 
+    # TODO
+end
+
