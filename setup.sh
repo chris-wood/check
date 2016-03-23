3a6bf1c3933d7c432c337b00745b844cb6bd392e
Christopher Wood <christopherwood07@gmail.com>
2016-03-23 07:51:03 -0700

    Style

--- setup.sh
+++ setup.sh
@@ -1,12 +1,12 @@
-#!/bin/zsh
+# #!/bin/zsh
 
 # Decoration (for zsh)
-decorate() 
-  functions[$1]='
-    () { check -l '$@' && '$functions[$1]'; } "$@"
-    local ret=$?
-    return $ret'
+#decorate() 
+#  functions[$1]='
+#    () { check -l '$@' && '$functions[$1]'; } "$@"
+#    local ret=$?
+#    return $ret'
 
 alias check="ruby `pwd`/src/check.rb"
 g() { args="'$*'"; check -l $args; git "$@"; }
-
+t() { args="'$*'"; check --todo $args; }
